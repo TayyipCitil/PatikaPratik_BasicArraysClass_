dd7ecf3dede6b3e100916b7caf20c22818ee266b
= <=>
2024-07-09 09:04:06 +0300

    version1

--- src/BasicArraysClass.java
+++ src/BasicArraysClass.java
@@ -36,13 +36,117 @@ public class BasicArraysClass {
 
 
     static int[] soort(int[] intList) {
-        int[] regularForm = intList;
-        for (int i = 0; i < intList.length-1; i++) {
-            for (int j = i+1; j < intList.length; j++) {
-
-
+        for (int i = 0; i < intList.length - 1; i++) {
+            for (int j = i + 1; j < intList.length; j++) {
+                if (intList[i] > intList[j]) {
+                    int a = intList[i];
+                    intList[i] = intList[j];
+                    intList[j] = a;
+                }
             }
         }
-        return regularForm;
+        return intList;
     }
+
+    //soort() metot için overloading yaptık
+    static String[] soort(String[] strList) {
+        String[] alphabet = {"a", "b", "c", "ç", "d", "e", "f", "g", "ğ", "h", "ı", "i", "j", "k", "l", "m", "n", "o", "ö", "p", "r", "s", "ş", "t", "u", "ü", "v", "y", "z"};
+        int[] indis = new int[strList.length];
+        for (int i = 0; i < strList.length; i++) {
+            for (int j = 0; j < alphabet.length; j++) {
+                if (strList[i] == alphabet[j]) {
+                    indis[i] = j;
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < indis.length - 1; i++) {
+            for (int j = i + 1; j < indis.length; j++) {
+                if (indis[i] > indis[j]) { //elamanların indislerini sıralarken kendilerini de sıralattık
+                    int a = indis[i];
+                    String b = strList[i];
+                    indis[i] = indis[j];
+                    strList[i] = strList[j];
+                    indis[j] = a;
+                    strList[j] = b;
+                }
+            }
+        }
+        return strList;
+    }
+
+
+    static int search(int[] intList, int value) {
+        int i;
+        for (i = 0; i < intList.length; i++) {
+            if (value == intList[i]) {
+                break;
+            } else if (i == intList.length - 1) {
+                System.out.println("Bu eleman dizide yok");//eleman olamyınca -1 dödürüyor
+                i = -1;
+            }
+        }
+        return i;
+    }
+
+    //search() metot için overloading yaptık
+    static int search(String[] intList, String value) {
+        int i;
+        for (i = 0; i < intList.length; i++) {
+            if (value == intList[i]) {
+                break;
+            } else if (i == intList.length - 1) {
+                System.out.println("Bu eleman dizide yok");//eleman olamyınca -1 dödürüyor
+                i = -1;
+            }
+        }
+        return i;
+    }
+
+
+
+    static int[] coopy(int[] intList, int toIndis) {
+        int[] copy = new int[toIndis + 1];
+        for (int i = 0; i < toIndis + 1; i++) {
+            copy[i] = intList[i];
+        }
+        return copy;
+    }
+
+    static int[] coopy(int[] intList, int fromIndex, int toIndex) {
+        int[] copy = new int[0];
+        if (fromIndex < 0 || toIndex > intList.length - 1 || fromIndex > toIndex) {
+            System.out.println("Hatalı giriş");
+        } else {
+            copy = new int[toIndex - fromIndex + 1];
+            for (int i = 0, j = fromIndex; j < toIndex + 1; i++, j++) {
+                copy[i] = intList[j];
+            }
+        }
+        return copy;
+    }
+
+    static String[] coopy(String[] intList, int toIndis) {
+        String[] copy = new String[toIndis + 1];
+        for (int i = 0; i < toIndis + 1; i++) {
+            copy[i] = intList[i];
+        }
+        return copy;
+    }
+
+    static boolean equalss(int[] intList1,int[] intList2){
+        boolean isEquals=false;
+        if(intList1.length==intList2.length){
+            for(int i=0;i< intList1.length;i++){
+                if(intList1[i]==intList2[i]){
+                    isEquals=true;
+                }else{
+                    isEquals=false;
+                    break;
+                }
+            }
+        }
+        return isEquals;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -8,22 +8,42 @@ public class Main {
         int[] intList4={-2,4,0,3,4,12,45,-3,44,56,-100};
         String[] strList1={"a","b","c","d","e"};
         String[] strList2={"a","b","c","d","e"};
-        String[] strList3={"c","e","f","l","m"};
+        String[] strList3={"c","z","a","l","m"};
 
 
         BasicArraysClass.tooString(intList1);
 
-        System.out.println("\n================");
+        System.out.print("\n================\n");
         BasicArraysClass.filll(intList3,10);
         BasicArraysClass.filll(intList3,1,3,4);
         BasicArraysClass.tooString(intList3);
 
-        System.out.println("\n================");
+        System.out.print("\n================\n");
         BasicArraysClass.tooString(strList1);
 
-        System.out.println("\n================");
+        System.out.print("\n================\n");
         BasicArraysClass.soort(intList4);
         BasicArraysClass.tooString(intList4);
+
+        System.out.print("\n================\n");
+        BasicArraysClass.soort(strList3);
+        BasicArraysClass.tooString(strList3);
+
+        System.out.print("\n================\n");
+        System.out.println(BasicArraysClass.search(intList2,5));
+        System.out.println(BasicArraysClass.search(strList2,"d"));
+
+        System.out.print("\n================\n");
+        BasicArraysClass.tooString(BasicArraysClass.coopy(intList2,2));
+        BasicArraysClass.tooString(BasicArraysClass.coopy(intList2,2,3));
+        BasicArraysClass.tooString(BasicArraysClass.coopy(strList2,3));
+
+        System.out.print("\n================\n");
+
+        System.out.println( BasicArraysClass.equalss(intList1,intList2));
+
+
+
 
 
     }
